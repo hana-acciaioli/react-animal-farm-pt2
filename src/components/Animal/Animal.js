28e76b327907c558ea93e6801bd08beec8e3eb69
Hana Acciaioli <hana.acciaioli@gmail.com>
2023-01-30 09:35:22 -0800

    props passing to animal.js and displaying on page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,9 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(props) {
+  return (
+    <div className="animal" style={{ top: props.top, left: props.left }}>
+      <img src={`animals/${props.type}.svg`}></img>
+      <h2 className="name">{props.name}</h2>
+      <p>{props.says}</p>
+    </div>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import './Main.css';
 import background from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main style={{ backgroundImage: `URL(${background})` }}></main>;
+  return (
+    <main style={{ backgroundImage: `URL(${background})` }}>
+      {animals.map((animal) => (
+        <Animal
+          key={animal.name}
+          name={animal.name}
+          says={animal.says}
+          type={animal.type}
+          top={animal.top}
+          left={animal.left}
+        />
+      ))}
+    </main>
+  );
 }
